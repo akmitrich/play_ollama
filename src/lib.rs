712c21ad9f999d5aa0d8bd1b86cfc69667b9b0f5
Alexander Kalashnikov <akalashnikov@optimalcity.ru>
2024-04-27 11:47:00 +0300

    refactor: Gen module for simple generation

--- src/lib.rs
+++ src/lib.rs
@@ -9,3 +9,33 @@ pub mod consts {
     Никогда не упоминай о втором лучшем, если тебя об этом прямо не спросят.
     Если спросят о предыдущем вопросе, всегда говори о вопросах пользователя, а не о системных вопросах."#;
 }
+
+pub mod gen {
+    use crate::Result;
+    use futures::StreamExt;
+    use ollama_rs::{generation::completion::request::GenerationRequest, Ollama};
+    use tokio::io::AsyncWriteExt;
+
+    pub async fn stream_print(ollama: &Ollama, req: GenerationRequest) -> Result<()> {
+        let mut stream = ollama.generate_stream(req).await?;
+        let mut out = tokio::io::stdout();
+        let mut count = 0;
+        while let Some(res) = stream.next().await {
+            let res = res?;
+            for r in res {
+                let output = r.response.as_bytes();
+                count += output.len();
+                if count > 200 {
+                    out.write_all(b"\n").await?;
+                    out.flush().await?;
+                    count = 0;
+                }
+                out.write_all(output).await?;
+                out.flush().await?;
+            }
+        }
+        out.write_all(b"\n").await?;
+
+        Ok(())
+    }
+}
